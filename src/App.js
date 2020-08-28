f1b3c103931d1f209c78a57726e0ca25937d4ec5
deanssmart <deanssmart@gmail.com>
2020-08-28 13:14:53 +0100

    HideMe component added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Multiplier from './components/Multiplier';
 import EvenClicks from './components/EvenClicks';
 import CountBy from './components/CountBy';
+import HideMe from './components/HideMe';
 
 const App = () => (
-  <>
+  <main className="container">
     <Multiplier x={ 5 } y ={ 7 } />
     <EvenClicks />
     <CountBy step={ 5 } />
-  </>
+    <HideMe>Blah blah blah</HideMe>
+  </main>
 );
 
 export default App;
