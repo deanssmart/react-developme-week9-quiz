2175f55da801dc1788ab35ca59bc181a9c7c74fe
deanssmart <deanssmart@gmail.com>
2020-08-28 14:42:43 +0100

    Indentation fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
     const Footer = () => (
         <footer className="fixed-bottom bg-dark">
             <Nav defaultActiveKey="/" as="ul">
-            <Nav.Item as="li">
-                <Link to="/" className="nav-link">Home</Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Link to="/even-clicks" className="nav-link">Even Clicks</Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Link to="/hide-me" className="nav-link">Hide Me</Link>
-            </Nav.Item>
+                <Nav.Item as="li">
+                    <Link to="/" className="nav-link">Home</Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Link to="/even-clicks" className="nav-link">Even Clicks</Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Link to="/hide-me" className="nav-link">Hide Me</Link>
+                </Nav.Item>
             </Nav>            
         </footer>    
     );
