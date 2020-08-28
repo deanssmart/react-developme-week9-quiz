0748aa9ba35e344254d8c03e68c6351ed01673d2
deanssmart <deanssmart@gmail.com>
2020-08-28 13:34:38 +0100

    MinimumLength Component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Multiplier from './components/Multiplier';
 import EvenClicks from './components/EvenClicks';
 import CountBy from './components/CountBy';
 import HideMe from './components/HideMe';
+import MinimumLength from './components/MinimumLength';
 
 const App = () => (
   <main className="container">
@@ -12,6 +13,7 @@ const App = () => (
     <EvenClicks />
     <CountBy step={ 5 } />
     <HideMe>Blah blah blah</HideMe>
+    <MinimumLength length={ 30 } />
   </main>
 );
 
---
+++ src/components/MinimumLength.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class MinimumLength extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { input: "" };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ input: e.currentTarget.value });
+    }
+
+    lengthCalc(length) {
+        const { input } = this.state;
+
+        return input.length < length ? "Too short!" : "";
+    }
+
+    render() {
+        const { input } = this.state;
+        const { length } = this.props;
+
+        return(
+            <form className="form-group">
+                <input 
+                    onChange={ this.handleChange }
+                    value={ input }
+                    className="form-control"
+                />
+                <p className="form-text text-muted">
+                    { this.lengthCalc( length ) }
+                </p>                  
+            </form>
+        );
+    }
+}
+
+export default MinimumLength;
