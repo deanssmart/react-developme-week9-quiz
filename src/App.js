fbf681cc99a4b19f3d4850fd87c1cf5526cb7af1
deanssmart <deanssmart@gmail.com>
2020-08-28 15:10:32 +0100

    404 page added with switch in routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Multiplier from './components/Multiplier';
 import EvenClicks from './components/EvenClicks';
@@ -9,32 +9,36 @@ import CountBy from './components/CountBy';
 import HideMe from './components/HideMe';
 import MinimumLength from './components/MinimumLength';
 import Footer from './components/Footer';
+import FourOhFour from './components/FourOhFour';
 
 const App = () => (
   <Router>
     <main className="container">
-      <Route exact path='/'>
-        <Multiplier x={ 5 } y ={ 7 } />
-        <EvenClicks />
-        <CountBy step={ 5 } />
-        <HideMe>Blah blah blah</HideMe>
-        <MinimumLength length={ 30 } />
-      </Route>
-      <Route path='/multiplier/:x/:y' render={ ({ match }) => (
-        <Multiplier x={ match.params.x } y ={ match.params.y } />
-      )} />
-      <Route path='/even-clicks'>
-        <EvenClicks />
-      </Route>
-      <Route path='/count-by/:step' render={ ({ match }) => (
-        <CountBy step={ match.params.step } />
-      )} />
-      <Route path='/hide-me'>
-        <HideMe>Blah blah blah</HideMe>
-      </Route>
-      <Route path='/minimum/:length' render={ ({ match }) => (
-        <MinimumLength length={ match.params.length } />
-      )} />
+      <Switch>
+        <Route exact path='/'>
+          <Multiplier x={ 5 } y ={ 7 } />
+          <EvenClicks />
+          <CountBy step={ 5 } />
+          <HideMe>Blah blah blah</HideMe>
+          <MinimumLength length={ 30 } />
+        </Route>
+        <Route path='/multiplier/:x/:y' render={ ({ match }) => (
+          <Multiplier x={ match.params.x } y ={ match.params.y } />
+        )} />
+        <Route path='/even-clicks'>
+          <EvenClicks />
+        </Route>
+        <Route path='/count-by/:step' render={ ({ match }) => (
+          <CountBy step={ match.params.step } />
+        )} />
+        <Route path='/hide-me'>
+          <HideMe>Blah blah blah</HideMe>
+        </Route>
+        <Route path='/minimum/:length' render={ ({ match }) => (
+          <MinimumLength length={ match.params.length } />
+        )} />
+        <FourOhFour />
+      </Switch>
     </main>
     <Footer />
   </Router>
---
+++ src/components/FourOhFour.js
@@ -0,0 +1,3 @@
+import React from "react";
+const FourOhFour = () => <p>Page not found</p>;
+export default FourOhFour;
